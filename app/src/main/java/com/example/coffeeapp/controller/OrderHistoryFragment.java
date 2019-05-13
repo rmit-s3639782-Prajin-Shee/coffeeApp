f3dbed006329c2f5c4250ea500934dca09f8d729
PrajinS <35509644+PrajinS@users.noreply.github.com>
2019-05-13 16:20:45 +1000

    - Project Upload

---
+++ app/src/main/java/com/example/coffeeapp/controller/OrderHistoryFragment.java
@@ -0,0 +1,34 @@
+package com.example.coffeeapp.controller;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.coffeeapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link OrderHistoryFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link OrderHistoryFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class OrderHistoryFragment extends Fragment {
+    public OrderHistoryFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_order_history, container, false);
+    }
+
+}
